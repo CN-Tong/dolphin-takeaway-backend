2276622f435e0ce96806bcaed4c6f3faa67b4d5d
CN-Tong <ZHT2018060708@163.com>
2024-01-01 23:52:54 +0800

    新增 店铺操作模块-设置店铺营业状态&获取店铺营业状态

---
+++ dolphin-server/src/main/java/com/tong/controller/admin/ShopController.java
@@ -0,0 +1,37 @@
+package com.tong.controller.admin;
+
+import com.tong.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController(value = "adminShopController")
+@RequestMapping("/admin/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    public static final String key = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @PutMapping("{status}")
+    @ApiOperation("设置店铺营业状态")
+    public Result setStatus(@PathVariable Integer status){
+        log.info("设置店铺营业状态，{}", status == 1 ? "营业中" : "打烊中");
+        redisTemplate.opsForValue().set(key, status);
+        return Result.success();
+    }
+
+    @GetMapping("/status")
+    @ApiOperation("获取店铺营业状态")
+    public Result getStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get(key);
+        log.info("获取店铺营业状态为，{}", status == 1 ? "营业中" : "打烊中");
+        return Result.success(status);
+    }
+}
